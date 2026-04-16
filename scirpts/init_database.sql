fdf7e6c6b1ef21158569616c67410d326c90f8a9
Brighton Gorova <brightongorova@gmail.com>
2026-04-16 14:05:11 +0300

    Modify init_database.sql to handle database recreation
    Drop and recreate the 'DataWareHouse' database if it exists.

--- scirpts/init_database.sql
+++ scirpts/init_database.sql
@@ -1,4 +1,13 @@
 Use master;
+GO 
+
+  -- drop and recreate the 'DataWareHouse' database 
+  IF EXISTS (SELECT 1 FROM sys.database WHERE name = 'DataWareHouse' )
+  BEGIN 
+    ALTER DATABASE DataWareHouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+    DROP DATABASE DataWareHouse
+  END;
+GO
 
 CREATE DATABASE DataWareHouse;
 
